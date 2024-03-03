868b564ff14f0e9a4e047e9ba5daf1808c08af1f
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-03-03 21:45:59 +0000

    Configuring communications between front and back

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to Djacket
+        </p>
+        <p class="subtitle">
+          The best jacket store online
+        </p>
+      </div>
+    </section>
+    <div class="column is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <div class ="column is-3" 
+      v-for="product in latestProducts" 
+      v-bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail"/>
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+          View Details
+        </div>
+    </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import axios from 'axios'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+    latestProducts:[]
+  }
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods:{
+    getLatestProducts(){
+      axios.get('/api/v1/latest-products/')
+      .then(response => {
+        this.latestProducts = response.data
+        console.log("Latest products: "+ this.latestProducts)
+        console.log("Latest products updated")
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
--- environment_3_10_13/djackets_django/djackets_django/settings.py
+++ environment_3_10_13/djackets_django/djackets_django/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",
+]
