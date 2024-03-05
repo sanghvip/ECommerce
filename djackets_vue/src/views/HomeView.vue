1cd53161109b66a00b2fe85a7bf7a226ef37dee7
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2024-03-05 01:00:59 +0000

    Setting the document title

--- djackets_vue/src/views/HomeView.vue
+++ djackets_vue/src/views/HomeView.vue
@@ -49,6 +49,7 @@ export default {
   },
   mounted() {
     this.getLatestProducts()
+    document.title = 'Home | Djackets'
   },
   methods:{
     async getLatestProducts(){
--- djackets_vue/src/views/ProductView.vue
+++ djackets_vue/src/views/ProductView.vue
@@ -50,6 +50,7 @@ export default {
             await axios.get(`/api/v1/products/${category_slug}/${product_slug}`)
             .then(response => {
                 this.product = response.data
+                document.title = this.product.name + ' | Djackets'
             })
             .catch(error => {
                 console.log(error)
